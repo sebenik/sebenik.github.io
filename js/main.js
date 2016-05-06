8672ef2948be6c368a0781cf80a782201b0e3ccd
Žiga Šebenik <ziga@sebenik.com>
2016-05-06 15:26:02 +0200

    Heading style changes and some image captions added

--- js/main.js
+++ js/main.js
@@ -1,40 +1,18 @@
 $(window).ready(function(){
-		/*
-	var player = document.getElementById('player_1');
-	  $f(player).addEvent('ready', ready);
-	   
-	    function addEvent(element, eventName, callback) {
-		    if (element.addEventListener) {
-			      element.addEventListener(eventName, callback, false)
-			    } else {
-				      element.attachEvent(eventName, callback, false);
-				    }
-		  }
-		 
-		  function ready(player_id) {
-		      var froogaloop = $f(player_id);
-		      froogaloop.addEvent('play', function(data) {
-			        $('.flexslider').flexslider("pause");
-			      });
-		      froogaloop.addEvent('pause', function(data) {
-			        $('.flexslider').flexslider("play");
-			      });
-		    }
-			*/
 
 	$('.flexslider').fitVids().flexslider({
 		animation: 'slide',
-		useCSS: false,
+		slideshow: false,
 		smoothHeight: true,
 		controlNav: true,
 		directionNav: true,
 		prevText: "",
 		nextText: "",
-		slideshow: false,
 		video: true,
+		useCSS: false,
+		touch: true,
 		before: function(slider){
 				var sliderID = slider.attr("id");
-		        //$f(player).api('pause');
 				if($("#" + sliderID + " #youtubevideo").get(0) !== undefined) {
 					var iframe = $("#" + sliderID + " #youtubevideo").get(0).contentWindow;
 					var func = 'pauseVideo';
@@ -42,4 +20,5 @@ $(window).ready(function(){
 				}
 		      }
 	});
+
 });
