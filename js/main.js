6ae4734b3e7c2158952634cea30882d50543496d
Žiga Šebenik <ziga@sebenik.com>
2016-05-12 18:27:57 +0200

    Image rescale and fix slider to fix height

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 $(window).ready(function(){
 
-	$('.flexslider').fitVids().flexslider({
-		animation: 'slide',
-		slideshow: false,
-		smoothHeight: true,
-		controlNav: true,
-		directionNav: true,
-		prevText: "",
-		nextText: "",
-		video: true,
-		useCSS: false,
-		touch: true,
-		before: function(slider){
-				var sliderID = slider.attr("id");
-				if($("#" + sliderID + " #youtubevideo").get(0) !== undefined) {
-					var iframe = $("#" + sliderID + " #youtubevideo").get(0).contentWindow;
-					var func = 'pauseVideo';
-					iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
-				}
-		      }
-	});
-
+  $('.flexslider').fitVids().flexslider({
+    animation: 'slide',
+    slideshow: false,
+    smoothHeight: false,
+    controlNav: true,
+    directionNav: true,
+    prevText: "",
+    nextText: "",
+    video: true,
+    useCSS: false,
+    touch: true,
+    before: function(slider){
+      var sliderID = slider.attr("id");
+      if($("#" + sliderID + " #youtubevideo").get(0) !== undefined) {
+        var iframe = $("#" + sliderID + " #youtubevideo").get(0).contentWindow;
+        var func = 'pauseVideo';
+        iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+      }
+    }
+  });
 });
+
